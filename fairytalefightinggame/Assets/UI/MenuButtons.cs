e722795d0ca241f78a3d07bae6690d63ea07059f
Felipe Gomez-Frittelli <fgomezfr@outlook.com>
2015-03-31 00:04:06 -0400

    folders, scene basics
    folders for characters; put anims, sounds, etc in folder for move, put
    custom scripts etc in top folder.
    
    main menu - in progress, lots of programmer art
    fighting stage - in progress, can add backgrounds to folder, basic
    health bars as sliders

---
+++ fairytalefightinggame/Assets/UI/MenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void LoadCharacterSelect()
+	{
+		Application.LoadLevel("CharacterSelect");
+	}
+
+	public void ShowControls()
+	{
+		// todo - figure out world camera, pan to page with controls
+		// easy out - load another level for controls
+	}
+
+	public void ShowCredits()
+	{
+		// todo - figure out world camera, pan to page with credits
+		// easy out - load another level for controls
+	}
+}
