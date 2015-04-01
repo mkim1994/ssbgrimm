75bb5a6863791fa886a1165989d3d5c9d5aa95a6
fgomezfr <fgomezfr@outlook.com>
2015-04-01 14:23:54 -0400

    stage setup, preliminary character loading

---
+++ fairytalefightinggame/Assets/LoadCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadCharacter : MonoBehaviour {
+
+	public int character;
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+	
+		// todo - find out how to pass values from character select
+		//        see Object.DontDestroyOnLoad()
+
+		// spawn a copy of the prefab for my character
+		GameObject myCharacter = (GameObject)Instantiate( prefabs[character], transform.position, Quaternion.identity );
+		// attach my character to me (it will now have my position)
+		myCharacter.transform.parent = transform;
+	}
+}
