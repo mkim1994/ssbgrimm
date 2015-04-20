ef9663fd8b33c6f239d2933e5e8e42d675cf2fbd
aasahay <a.amal.sahay@gmail.com>
2015-04-20 11:57:53 -0400

    Heart Projectile Beta

---
+++ fairytalefightinggame/Assets/Projectiles.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectiles : MonoBehaviour {
+
+	public Rigidbody2D heart;
+	private GameObject clone;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Y)) {
+			Invoke("Fire", 0.25f);
+		}
+	}
+
+	void Fire() {
+		Rigidbody2D g = Instantiate (heart,
+		                             transform.position + (new Vector3 (0.45f * transform.localScale.x,
+		                                                                -0.317f,
+		                                                                0f)),
+		                             transform.rotation) as Rigidbody2D;
+		clone = g.gameObject;
+		clone.SetActive(true);
+		g.velocity = transform.TransformDirection(new Vector3 (5 * transform.localScale.x, 0, 0));
+		Destroy (clone, 1.5f);
+	}
+}
